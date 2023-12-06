f0772553961c1f2408be0552be5e59d71053c3c1
Fabioprado01 <153131292+Fabioprado01@users.noreply.github.com>
2023-12-06 17:45:54 -0300

    Update exercicio_11.dart
    Proposta de alteração no código.
    Adoro seus exercícios!

--- fundamentos/exercicios/exercicio_11.dart
+++ fundamentos/exercicios/exercicio_11.dart
@@ -7,15 +7,20 @@ import 'dart:io';
    */
 
 main() {
-    const valLigMin = 1.15;
-    const valLigMinAdc = 0.26;
+  print("Digite a duração total da chamada em minutos:");
+  String? input = stdin.readLineSync();
+  int chamada = int.parse(input!); //o valor "imput" deve ser diferente e nulo.
+  
+  const chamadaSimples = 1.15; // definindo a constante de chamada simples.
+  const chamadaAdicional = 0.26; // definindo a constante do valor adicional 
 
-    stdout.write("Informe a duração da chamada em minutos: ");
-    var entrada = stdin.readLineSync();
-    var duracLig = int.parse(entrada);
-
-    double custLigac = ((duracLig - 1) * valLigMinAdc) + valLigMin;
-
-    print("O valor da sua ligação foi de R\$${custLigac}");
-    
+  if (chamada >0 && chamada <= 3){ // se chamada for menor ou iguala 3
+    double valor1 = chamadaSimples; // será o valor de chamada simples
+    print ("O valor da sua chamada é de $valor1 reais.");
+  } else if (chamada >= 4){ // se a chamada for maior ou igual a 4
+    double valor2 = ((chamada - 3) * chamadaAdicional) + chamadaSimples; //calculo do valor simples mais o adicional
+    print("O valor da sua chamada é de $valor2 reais."); 
+    } else if (chamada <= 0) {
+    print("Você não fez nenhuma chamada.");
+    }
 }
